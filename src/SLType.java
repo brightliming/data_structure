87be5dfa4313e47379e9c9d33e2f4f5a2f590fc3
bright <limingbright@yahoo.com>
2016-07-13 13:55:54 +0800

    init

---
+++ src/SLType.java
@@ -0,0 +1,26 @@
+/**
+ * Created by bright on 16-7-13.
+ */
+
+class SDATA{
+    String key;
+    String value;
+}
+
+/**
+ * 顺序表
+ */
+public class SLType {
+    final int MAX_LENGTH = 50;
+    SDATA[] DataList = new SDATA[MAX_LENGTH+1];
+    int SLength;
+
+    void initSLType(){
+        SLength = 0;
+    }
+
+    void addSLType(){
+
+    }
+
+}
