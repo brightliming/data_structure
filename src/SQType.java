e3bfffca878d8197df8aab092c95263be9b97608
bright <limingbright@yahoo.com>
2016-07-15 10:21:26 +0800

    stack queue

--- src/SQType.java
+++ src/SQType.java
@@ -1,5 +1,66 @@
 /**
  * Created by bright on 16-7-13.
  */
+class DATA4{
+    String key;
+    String value;
+}
 public class SQType {
+    static final int QUEUELEN = 10;
+
+    DATA4[] data = new DATA4[QUEUELEN];
+
+    int head = 0;
+    int tail = 0;
+
+    SQType init(){
+        SQType p;
+        if((p=new SQType())==null){
+            p.head = 0;
+            p.tail = 0;
+            return p;
+        }
+        return null;
+    }
+
+    boolean SQIsEmpty(){
+        return head==tail;
+    }
+
+    boolean SQIsFull(){
+        return (tail-head)==QUEUELEN;
+    }
+
+    void SQTypeClear(){
+        head = 0;
+        tail = 0;
+    }
+    int inSQType(DATA4 _data){
+        if(SQIsFull()){
+            System.out.println("队列已满");
+            return 0;
+        }
+        data[tail++] = _data;
+        return 1;
+    }
+
+    DATA4 outSQType(){
+        if(SQIsEmpty()){
+            System.out.println("空队列");
+            return null;
+        }
+        return data[head++];
+    }
+
+    DATA4 peekSQType(){
+        if(SQIsEmpty()){
+            System.out.println("空队列");
+            return null;
+        }
+        return data[head];
+    }
+
+    int SQTypeLen(){
+        return tail - head;
+    }
 }
--- src/StackType.java
+++ src/StackType.java
@@ -1,5 +1,64 @@
 /**
  * Created by bright on 16-7-13.
  */
+class DATA3{
+    String key;
+    String value;
+}
 public class StackType {
+    static final int MAXLEN = 50;
+
+    DATA3[] data = new DATA3[MAXLEN+1];
+
+    int top;
+
+    StackType STInit(){
+        StackType p;
+        if((p=new StackType())!=null){
+            p.top = 0;
+            return p;
+        }
+
+        return null;
+    }
+
+    boolean STIsEmpty(){
+        return top == 0;
+    }
+
+    boolean STIsFull(){
+        return top == MAXLEN;
+    }
+
+    void STClean(){
+        top = 0;
+    }
+
+    int pushST(DATA3 data){
+        if(top==MAXLEN){
+            System.out.println("栈溢出");
+            return 0;
+        }
+
+        this.data[++top] = data;
+        return 1;
+    }
+
+    DATA3 pullST(){
+        if(top==0){
+            System.out.println("空栈");
+            return null;
+        }
+        return data[top--];
+    }
+
+    DATA3 peekST(){
+        if(top==0){
+            System.out.println("空栈");
+            return null;
+        }
+        return data[top];
+    }
+
+
 }
